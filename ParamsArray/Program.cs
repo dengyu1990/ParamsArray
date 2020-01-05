5df5508b864cba21c6f82a15b7b35c75a55fd91c
Deng Yu <jardy@vip.qq.com>
2020-01-05 23:05:07 +0800

    添加项目文件。

---
+++ ParamsArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void doWork()
+        {
+            //Console.WriteLine(Util.Sum(null));
+            //Console.WriteLine(Util.Sum());
+            Console.WriteLine(Util.Sum(43, 44, 23, 65, 23));
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
